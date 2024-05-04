926d99aae3e97ef6bb093bd79632be9cb06d9972
Kaushik Roy <63772738+kaus-2103@users.noreply.github.com>
2024-05-04 13:53:25 +0600

    erasing DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_02_133944) do
-  create_table "drawings", force: :cascade do |t|
-    t.integer "x"
-    t.integer "y"
-    t.string "state"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.1].define(version: 0) do
 end
